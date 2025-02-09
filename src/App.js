ebeca8cfcfae5ee696c0299e97afb6f6eb90d1e6
elsanaa1 <sanaa.jerbouh1@gmail.com>
2025-02-09 18:21:47 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+
+
+
+function App() {
+   const [movies,setMovies]=useState([{
+    title:'Avatar',
+     description:'Avatar is the story of Jake Sully, a paraplegic marine, who replaces his brother on a secret mission to infiltrate the Navi, the colony of beings that sit on the planet of Pandora, where there is a precious ore, that sells at a ridiculous amount.',
+      posterURL:'',
+       rating:'4.3'
+   },{
+    title:'Avatar',
+    description:'Avatar is the story of Jake Sully, a paraplegic marine, who replaces his brother on a secret mission to infiltrate the Navi, the colony of beings that sit on the planet of Pandora, where there is a precious ore, that sells at a ridiculous amount.',
+     posterURL:'',
+      rating:'4.3'
+   },{
+    title:'Avatar',
+    description:'Avatar is the story of Jake Sully, a paraplegic marine, who replaces his brother on a secret mission to infiltrate the Navi, the colony of beings that sit on the planet of Pandora, where there is a precious ore, that sells at a ridiculous amount.',
+     posterURL:'',
+      rating:'4.3'
+   }])
+  return(
+    <div >
+    
+    </div>
+  );
+
+}
+
+
+export default App;
